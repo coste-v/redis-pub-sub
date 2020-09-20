ff75d29b0f201eeb00feab1efed4560871324d90
kofi-kari-kari <kofi_kari_kari@hotmail.com>
2020-09-20 21:52:27 +0200

    [master] worker with queue example added

--- worker/task/main.py
+++ worker/task/main.py
@@ -8,7 +8,12 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-if __name__ == "__main__":
+def pubsub_example():
+    """
+        Here, if a message is send to REDIS_CHANNEL,
+        all the worker listening to this channel will receive
+        the message
+    """
     redis = Redis(
         host=os.getenv("REDIS_HOST"),
         port=int(os.getenv("REDIS_PORT")),
@@ -23,3 +28,20 @@ if __name__ == "__main__":
         if message:
             logger.info(message["data"])
         time.sleep(0.01)
+
+def queue_example():
+    redis = Redis(
+        host=os.getenv("REDIS_HOST"),
+        port=int(os.getenv("REDIS_PORT")),
+        decode_responses=True
+    )
+
+    while True:
+        message = redis.rpop(os.getenv("REDIS_CHANNEL"))
+        if message:
+            logger.info(message)
+        time.sleep(0.01)
+
+
+if __name__ == "__main__":
+    queue_example()
